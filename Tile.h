dcf3dff116df86064d1fd4cbed604f2e743c30d0
Robert-Gonzalez <robertgonzalez.me@gmail.com>
2017-07-29 16:02:59 -0400

    Created Tile Class

--- Tile.h
+++ Tile.h
@@ -1,6 +1,24 @@
 using namespace std;
 
+#include <iostream>
+#include "Piece.h"
+
 class Tile {
 public:
-	public Tile();
+	Tile();
+
+	bool isPieceOnTile();
+
+	void addPieceOnTile(Piece* newPiece);
+
+	Piece* removePieceFromTile();
+
+	~Tile();
+
+
+private:
+	bool pieceOnTile_;
+
+	Piece* piece_;
+
 };
